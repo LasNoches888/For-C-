c0cc24d4c8c787c354bc046503ef7de86be5e94e
Evgeniy <e.koskov@bk.ru>
2024-11-22 18:20:13 +0500

    function23

--- main.cpp
+++ main.cpp
@@ -10,11 +10,18 @@ void readfromfile(vector<string>& lines) {
 }
 
 void printlines(const vector<string>& lines) {
-
+    for (const auto& line:lines) {
+        cout << line << endl;
+    }
 }
 
 void writetowile(const vector<string>& lines) {
-
+    ofstream out;
+    out.open("file2.txt");
+    for (const auto& line : lines){
+        out << line << endl;
+    }
+    out.close();
 }
 
 int main() {
